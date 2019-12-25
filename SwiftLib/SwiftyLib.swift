0f84cbccfa93ee700407dc60b29ff41d2f017cd2
naveen murthy <naveen.murthy@in.ibm.com>
2019-12-25 22:01:13 +0530

    Added SwiftyLib.podspec

---
+++ SwiftLib/SwiftyLib.swift
@@ -0,0 +1,24 @@
+//
+//  SwiftyLib.swift
+//  SwiftLib
+//
+//  Created by Prasad Pallela on 25/12/19.
+//  Copyright © 2019 Prasad Pallela. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyLib
+{
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int
+    {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int
+    {
+        return a - b
+    }
+}
---
+++ SwiftyLibExamples/Classes/TestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TestViewController.swift
+//  SwiftLib
+//
+//  Created by Prasad Pallela on 25/12/19.
+//  Copyright © 2019 Prasad Pallela. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
